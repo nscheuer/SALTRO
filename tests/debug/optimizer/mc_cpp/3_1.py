fb15d8cbe2f051323b8f1e47113a8e651fbc6439
nscheuer <niclas_scheuer@web.de>
2026-06-08 20:14:27 +0200

    speed optimizations

--- tests/debug/optimizer/mc_cpp/3_1.py
+++ tests/debug/optimizer/mc_cpp/3_1.py
@@ -32,10 +32,10 @@ def create_planner_settings():
 
     plannersettings.num_passes = 1
     plannersettings.passes[0].dt = DT_SECONDS
-    plannersettings.passes[0].ilqr.cost_tol = 1e-5
+    plannersettings.passes[0].ilqr.cost_tol = 1e-3
     plannersettings.passes[0].ilqr.max_iters = 20
 
-    plannersettings.passes[0].auglag.max_outer_iters = 30
+    plannersettings.passes[0].auglag.max_outer_iters = 20
     plannersettings.passes[0].auglag.constraint_tol = 1e-3
 
     cost = plannersettings.passes[0].cost
